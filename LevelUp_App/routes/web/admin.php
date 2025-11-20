1136cefdd597001270844c40ad2e9e5ab754ca8c
Gabbo693 <gabrielesolazzo43@gmail.com>
2025-11-20 13:11:35 +0100

    routes for admin

--- LevelUp_App/routes/web/admin.php
+++ LevelUp_App/routes/web/admin.php
@@ -12,11 +12,19 @@ return function () {
         ->name('admin.')
         ->group(function () {
             Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
-
             Route::post('/users', [AdminUserController::class, 'store'])->name('users.store');
             Route::patch('/users/{user}', [AdminUserController::class, 'update'])->name('users.update');
             Route::patch('/users/{user}/promote', [AdminUserController::class, 'promote'])->name('users.promote');
             Route::patch('/users/{user}/demote', [AdminUserController::class, 'demote'])->name('users.demote');
             Route::delete('/users/{user}', [AdminUserController::class, 'destroy'])->name('users.destroy');
         });
+
+    Route::middleware(IsAdmin::class)->prefix('admin')->name('admin.')->group(function () {
+            Route::post('/rewards', [AdminRewardsController::class, 'store'])->name('rewards.store');
+            Route::put('/rewards/{reward}', [AdminRewardsController::class, 'update'])->name('rewards.update');
+            Route::patch('/rewards/{reward}/archive', [AdminRewardsController::class, 'archive'])->name('rewards.archive');
+            Route::patch('/rewards/{reward}/unarchive', [AdminRewardsController::class, 'unarchive'])->name('rewards.unarchive');
+            Route::delete('/rewards/{reward}', [AdminRewardsController::class, 'destroy'])->name('rewards.destroy');
+        });
 };
+
